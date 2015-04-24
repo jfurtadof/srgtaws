41251ab2af9fa8e79c080ab741465f7c7d29a267
Jessica Furtado <jessica@student.dei.uc.pt>
2015-04-24 02:04:18 +0100

    up conexão ok

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@
 var base_url = "http://ws.audioscrobbler.com/2.0/"
 
 // chave API
-var apikey = "d12e1581683ed053db4bbe44c615ac37";
+var apikey = "5a429fc03f1015f9fe983a57b5d54e97";
 
 // URL base para pedidos à API do Last.fm - concatenar em cada pedido com nome do método a invocar e parâmetros do mesmo
-var base_request = "http://ws.audioscrobbler.com/2.0/?api_key=d12e1581683ed053db4bbe44c615ac37&format=json&method=";
+var base_request = "http://ws.audioscrobbler.com/2.0/?api_key=5a429fc03f1015f9fe983a57b5d54e97&format=json&method=";
 
 
 // Nome do utilizador a analisar
@@ -16,9 +16,7 @@ var amigos = {};
 
 var banda;
 
-
-function getUserInfo()
-{
+function getUserInfo(){
 	var data = {
         api_key: apikey,
         method: "user.getInfo",
@@ -26,23 +24,20 @@ function getUserInfo()
         format: "json"
     };
 
-		alert ("antes do ajax");
-    $.get(base_url, data).done(processUserInfo).fail(logError("obter info do utilizador "+username));
-		alert(username);
+  $.get(base_url, data).done(processUserInfo);
 }
 
-
 function logError() {
-	alert (this);
+alert("nop");
 }
 
 function log () {
 	alert (this);
 }
 
-function processUserInfo(info)
-{
-	if(info.error){
+function processUserInfo(info){
+	//alert(info);
+if(info.error){
 		log("Erro: "+info.message);
 		searchAgain();
 	}
@@ -53,8 +48,9 @@ function processUserInfo(info)
 	var url = info.user.url;
 	var local = info.user.country;
 
-	//var tabela = document.getElementById("tabela");
-	//$('#tabela').html("<a href='"+url+"'>"+name+"</a><br>"+age+" years old<table></table>");
 
-	}
+	var tabela = document.getElementById("tabela");
+	$("#tabela").html("<p>Entrou processUserInfo</p>");
+	$('#tabela').html("<a href='"+url+"'>"+name+"</a><br>"+age+" years old<table></table>");
+}
 }
--- profile.php
+++ profile.php
@@ -1,17 +1,15 @@
 <html>
 <head>
-<title>CONFORT ZONE</title>
+<title>FACIO</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <script type="text/javascript" src="jquery-1.11.0.min.js"></script>
-
 <!-- Processing.js -->
-<script type="text/javascript" src="processing-1.4.1.min.js"></script>
+<!--script type="text/javascript" src="processing-1.4.1.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="style.css">-->
 
 <script type="text/javascript" src="javascript.js"></script>
-
-<link rel="stylesheet" type="text/css" href="style.css">
-
 
 </head>
 <body>
@@ -24,22 +22,17 @@ $(document).ready(function(){
 
    //alert(username2);
    //document.write(username2);
-   procura();
+//   procura();
 
 });
 </script>
-<div class="fundo">
-		<div id="toPjs">
-			<canvas id="pjs" data-processing-sources="background/background.pde"></canvas>
-		</div>
-</div>
+
 <div class="corpo">
 
 	<div id="tabela">
 	</div>
-	<div id="tags"></div>
 
-	<div class="dados">
+<!--	<div class="dados">
 		<div id="quad"></div>
 		<div id="amigos"></div>
 		<div id="vizinhos"></div>
@@ -49,13 +42,12 @@ $(document).ready(function(){
 		<div id="insert"></div>
 
 		<div id="toPjs2">
-			<!-- <canvas id="pjs2" data-processing-sources="zonadeconforto/zonadeconforto.pde" style="width: 100%; height: 1200px;  margin-top: 0px; padding-top: 0px;" ></canvas>
-			-->
+			 <canvas id="pjs2" data-processing-sources="zonadeconforto/zonadeconforto.pde" style="width: 100%; height: 1200px;  margin-top: 0px; padding-top: 0px;" ></canvas>
+
 			<canvas id="pjs2" data-processing-sources="zona/zona.pde"></canvas>
 		</div>
-	</div>
+	</div>-->
 </div>
-
 
 </body>
 </html>
